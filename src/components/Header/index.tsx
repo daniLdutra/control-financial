ed59081bc512c88619381791424aef441fe5e920
Daniella Dutra <dani.ldutra@gmail.com>
2022-09-05 13:00:02 -0300

    refator: correcting indentation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,14 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
-import logoImg from '../../assets/logo.svg'
+import logoImg from '../../assets/logo.svg';
 
-export function Header(){
-  return(<HeaderContainer>
-    <HeaderContent>
-      <img src={logoImg} alt='' />
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
 
-      <NewTransactionButton>Nova Transação</NewTransactionButton>
-    </HeaderContent>
-  </HeaderContainer>)
+        <NewTransactionButton>Nova Transação</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  );
 }
