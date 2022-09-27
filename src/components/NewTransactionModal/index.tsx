b4020ea6383973a2a9303ee7f04d86cf2073f8c4
Daniella Dutra <dani.ldutra@gmail.com>
2022-09-27 11:31:28 -0300

    feature: modal structure for new transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,12 +1,27 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import { X } from 'phosphor-react';
+import { CloseButton, Content, Overlay } from './styles';
+
 export function NewTransactionModal() {
   return (
     <div>
       <Dialog.Portal>
-        <Dialog.Overlay />
-        <Dialog.Content>
+        <Overlay />
+        <Content>
           <Dialog.Title>Nova Transação</Dialog.Title>
-          <Dialog.Close />
-        </Dialog.Content>
+
+          <CloseButton>
+            <X />
+          </CloseButton>
+
+          <form action="">
+            <input type="text" placeholder="Descrição" required />
+            <input type="number" placeholder="Preço" required />
+            <input type="text" placeholder="categoria" required />
+
+            <button type="submit">Cadastrar</button>
+          </form>
+        </Content>
       </Dialog.Portal>
     </div>
   );
