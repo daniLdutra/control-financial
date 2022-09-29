8547a3d4f9273f6780df8327f0908f9ea436981b
Daniella Dutra <dani.ldutra@gmail.com>
2022-09-29 12:53:59 -0300

    wip: applied formState to evaluate form submission state

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -11,11 +11,12 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
-  const { register, handleSubmit} = useForm<SearchFormInputs>({
+  const { register, handleSubmit, formState: {isSubmitted}} = useForm<SearchFormInputs>({
     resolver: zodResolver(searchFormSchema)
   });
 
-  function handleSearchTransactions(data: SearchFormInputs) {
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await new Promise(resolve => setTimeout(resolve, 2000))
     //console.log(data);
   }
 
@@ -27,7 +28,7 @@ export function SearchForm() {
         {...register('query')}
       />
 
-      <button type="submit">
+      <button type="submit" disabled={isSubmitted}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
