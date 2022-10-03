e50a925cd7b4dbd800c292b4ec251836d847f5aa
Daniella Dutra <dani.ldutra@gmail.com>
2022-10-03 11:22:36 -0300

    feature: applied control to radiogroup type external component

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -8,25 +8,29 @@ import {
   TransactionType,
 } from './styles';
 import * as z from 'zod';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 const newTransactionFormSchema = z.object({
   description: z.string(),
   price: z.number(),
   category: z.string(),
- // type: z.enum(['income', 'outcome']),
+  type: z.enum(['income', 'outcome']),
 });
 
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
   const {
+    control,
     register,
     handleSubmit,
-    formState: { isSubmitted },
+    formState: { isSubmitting },
   } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormSchema),
+    defaultValues:{
+      type: 'income'
+    }
   });
 
   async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
@@ -66,18 +70,25 @@ export function NewTransactionModal() {
               required
             />
 
-            <TransactionType>
-              <TransactionButton variant="income" value="income">
-                <ArrowCircleUp size={24} />
-                Entrada
-              </TransactionButton>
-              <TransactionButton variant="outcome" value="outcome">
-                <ArrowCircleDown size={24} />
-                Saída
-              </TransactionButton>
-            </TransactionType>
-
-            <button type="submit" disabled={isSubmitted}>
+            <Controller
+              control={control}
+              name="type"
+              render={({field}) => {
+                return (
+                  <TransactionType onValueChange={field.onChange} value={field.value}>
+                    <TransactionButton variant="income" value="income">
+                      <ArrowCircleUp size={24} />
+                      Entrada
+                    </TransactionButton>
+                    <TransactionButton variant="outcome" value="outcome">
+                      <ArrowCircleDown size={24} />
+                      Saída
+                    </TransactionButton>
+                  </TransactionType>
+                );
+              }}
+            />
+            <button type="submit" disabled={isSubmitting}>
               Cadastrar
             </button>
           </form>
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -11,7 +11,7 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
-  const { register, handleSubmit, formState: {isSubmitted}} = useForm<SearchFormInputs>({
+  const { register, handleSubmit, formState: {isSubmitting}} = useForm<SearchFormInputs>({
     resolver: zodResolver(searchFormSchema)
   });
 
@@ -28,7 +28,7 @@ export function SearchForm() {
         {...register('query')}
       />
 
-      <button type="submit" disabled={isSubmitted}>
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
